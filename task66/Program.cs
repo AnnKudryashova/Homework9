5d19d3766147c0b58f35b128a3eb29cf24665a16
Anna Kudryashova <kisstochka23@mail.ru>
2022-09-06 23:04:36 +0500

    Задача 66 решена

---
+++ task66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+//в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+int SumNaturalsDigits(int n, int m)
+{
+    if (n == m) return n;
+    else return SumNaturalsDigits(n, m-1) + m;
+}
+
+int n = 4;
+
+int m = 8; 
+
+Console.WriteLine(SumNaturalsDigits(n,m));
